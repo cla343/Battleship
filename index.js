62dbed261c215a6040491ef1346ec4c6dbe5e8ef
Cory <coryadams94@yahoo.com>
2025-12-18 16:06:49 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+class Ship {
+    constructor(length){
+        this.length = length;
+        this.hits = 0;
+        this.sunk = false;
+    }
+
+//did the attack coordinates === opponents ship position
+    hit(){
+        this.hits++;
+        this.isSunk();
+    }
+
+//does the hitCount === length ; if so sunk
+    isSunk(){
+        if(this.hits>=this.length){
+            this.sunk = true;
+            return true;
+            }
+        return false;
+        }
+    }
+
+//Gameboards should keep track of missed attacks so they can display them properly.
+//Gameboards should be able to report whether or not all of their ships have been sunk.
+class Gameboard{
+    constructor(){
+
+    }
+
+//takes a pair of coordinates, determines whether or not the attack hit a ship and then sends the ‘hit’ function to the correct ship, or records the coordinates of the missed shot.
+    receiveAttack(shipCoordinate, attackCoordinate){
+
+    }
+}
+
+//There will be two types of players in the game, ‘real’ players and ‘computer’ players.
+//Each player object should contain its own gameboard.
+class Player{
+    constructor(){
+
+    }
+}
+
+module.exports = Ship; //exporting for tests
